c1e5a8cc42617679432f18968ec8923bb36ff821
Adel <adel.aref16@gmail.com>
2018-08-05 15:46:09 +0200

    third commit

--- Mind/ViewController.swift
+++ Mind/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    var asd = "asd"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
